709aa0b00fe51f6befff928ac2dde17fcfe1d0b4
eveyy999 <157994709+eveyy999@users.noreply.github.com>
2024-01-28 22:12:59 +0100

    Add files via upload

---
+++ function_remind_me.R
@@ -0,0 +1,9 @@
+remind_me <- function() {
+  messages <- c(
+    "Reminder: Submit the programming assignment by Friday.",
+    "This week's shopping list: milk, napkin, eggs, apples.",
+    "Pick a birthday gift for Xiangyu next week!",
+    "Study for next week's exam!"
+  )
+  return(messages)
+}
